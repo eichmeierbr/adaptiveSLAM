876fb1ed86dad2e25488a7939f357ce503ca3c53
shaun-cmu <60110416+Shaun-CMU@users.noreply.github.com>
2021-03-18 14:59:01 -0400

    added feature sensor and display now shows sensor readings

--- code/feature_sensor.py
+++ code/feature_sensor.py
@@ -1,6 +1,8 @@
-import numpy as np
+# feature_sensor
+from base_sensor import *
 
-class Base_sensor:
+
+class feature_sensor(Base_sensor):
     def __init__(self, local=True, in_map=np.zeros([1000,1000]), P_est=0.1, P_des=0.1, dim=2, freq=0.01):
         self._local = False
         self.__P_map = np.ones_like(in_map) * P_des
@@ -9,8 +11,15 @@ class Base_sensor:
         self._last_meas = -10
         self._dim = dim
 
+        self.features = np.array([[300,200],[200,150],[350,150]])
+
+    def set_values(self,feature):
+        self.features = feature
+
+
     def get_values(self):
-        return 
+        return self.features
+
 
 
     def getMeasure(self, env, robot):
@@ -20,7 +29,7 @@ class Base_sensor:
         \param robot    Robot object containing state information
         \param zt       Sensor measurement
         """
-
+        #TODO: change to sensor values we need
         X_t = robot._true_pose
         P_true =self.__P_map[X_t[0], X_t[1]]
         zt = X_t + np.random.normal(P_true)
--- code/simple_sim.py
+++ code/simple_sim.py
@@ -2,13 +2,22 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 from base_sensor import *
+from feature_sensor import *
 from robot import *
 from control import *
 
-def display_map(env, robot, dt):
+def display_map(env, robot, dt, sensor):
     plt.imshow(env, cmap='Greys')
     plt.scatter(robot._true_pose[0], robot._true_pose[1])
     plt.scatter(robot._est_pose[0], robot._est_pose[1])
+
+    for sens in sensors:
+        sensor_vals = sens.get_values()
+        if sensor_vals is None:
+            continue
+        else:
+            plt.scatter(sensor_vals[:,0], sensor_vals[:,1])
+    
 
     plt.draw()
     plt.pause(dt)
@@ -22,7 +31,7 @@ if __name__ == "__main__":
     init_pose = np.array([100,100])
 
     ## Initialize Map
-    env = np.loadtxt('maps/empty_map.csv', delimiter=',')
+    env = np.loadtxt('../maps/empty_map.csv', delimiter=',')
 
 
     ## Initialize Robot
@@ -32,12 +41,13 @@ if __name__ == "__main__":
     ## Initialize Sensors
     sensors = []
     sensors.append(Base_sensor())
+    sensors.append(feature_sensor())
 
     ## Initailze Path
     path = np.array([[0,0,0,250,150,0,0,-150,-250,0,0,150],
                      [0,100,100,0,0,-100,-100,0,0,100,100,0]]).T
 
-    display_map(env, robot, dt)
+    display_map(env, robot, dt, sensors)
 
     ## For time duration (Or all actions)
     for act in path:
@@ -55,6 +65,6 @@ if __name__ == "__main__":
         ## TODO: SLAM
 
         ## Display map
-        display_map(env, robot, dt)
+        display_map(env, robot, dt, sensors)
 
 

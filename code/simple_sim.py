0c408b6d56a8ae2f841e11466c42995f05f22916
shaun-cmu <60110416+Shaun-CMU@users.noreply.github.com>
2021-03-18 23:49:38 -0400

    added hotfix for floating point error

--- code/simple_sim.py
+++ code/simple_sim.py
@@ -6,6 +6,9 @@ from feature_sensor import *
 from robot import *
 from control import *
 
+
+from math import log
+
 def display_map(env, robot, dt, sensor):
     plt.imshow(env, cmap='Greys')
     plt.scatter(robot._true_pose[0], robot._true_pose[1])
@@ -23,7 +26,16 @@ def display_map(env, robot, dt, sensor):
     plt.pause(dt)
     plt.clf()
 
+    
+
 def get_path(waypoints,step_size=10):
+
+    ##np.arange has an error with floating point error
+    #solution is to use linspace but might take more work
+    #also path is returned as in as the smallest value
+
+    temp_correction_for_error = 0.01*step_size
+
     pathx = [waypoints[0,0]]
     pathy = [waypoints[0,1]]
 
@@ -32,14 +44,14 @@ def get_path(waypoints,step_size=10):
         ly = len(pathy)
 
         if pathx[lx-1]<waypoints[i+1,0]:
-            pathx = np.append(pathx,np.arange(pathx[lx-1],waypoints[i+1,0],step=step_size))
+            pathx = np.append(pathx,np.arange(pathx[lx-1],waypoints[i+1,0]+temp_correction_for_error,step_size))
         else:
-            pathx = np.append(pathx,np.flip(np.arange(waypoints[i+1,0],pathx[lx-1],step=step_size)))
+            pathx = np.append(pathx,np.flip(np.arange(waypoints[i+1,0],pathx[lx-1]+temp_correction_for_error,step_size)))
 
         if pathy[ly-1]<waypoints[i+1,1]:
-            pathy = np.append(pathy,np.arange(pathy[ly-1],waypoints[i+1,1],step=step_size))
+            pathy = np.append(pathy,np.arange(pathy[ly-1],waypoints[i+1,1]+temp_correction_for_error,step_size))
         else:
-            pathy = np.append(pathy,np.flip(np.arange(waypoints[i+1,1],pathy[ly-1],step=step_size)))
+            pathy = np.append(pathy,np.flip(np.arange(waypoints[i+1,1],pathy[ly-1]+temp_correction_for_error,step_size)))
         lx = len(pathx)
         ly = len(pathy)
 
@@ -48,9 +60,6 @@ def get_path(waypoints,step_size=10):
 
         if ly>lx:
             pathx = np.append(pathx,np.ones((ly-lx,1))*pathx[lx-1])
-
-            
-
 
     path = np.zeros((len(pathx),2))
     path[:,0] = pathx
